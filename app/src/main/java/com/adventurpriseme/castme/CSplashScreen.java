d1e9730010cae49498c6250d1b75e898c6dc79dd
tbremm <juggling.engineer@gmail.com>
2015-01-19 18:57:18 -0500

    Added a splash screen to the app. Needs work graphically, but it's there to be used. Right now, it's on a hard-coded 3-second timer. We can change that, or change it to an async task and use it to load resources from the internet or whatever. The splash screen activity needs to be the launcher activity, hence the manifest changes. It then launches the main activity.

---
+++ app/src/main/java/com/adventurpriseme/castme/CSplashScreen.java
@@ -0,0 +1,41 @@
+package com.adventurpriseme.castme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Class used to display a splash screen.
+ * <p/>
+ * Created by Timothy on 1/19/2015.
+ * Copyright 1/19/2015 adventurpriseme.com
+ */
+public class CSplashScreen
+	extends Activity
+	{
+	private static int SPLASH_TIME_OUT = 3000;  // Splash screen timeout (ms)
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState)
+		{
+		super.onCreate (savedInstanceState);
+		setContentView (R.layout.activity_splash_screen);
+		// Handle showing the splash screen and then launching the real main activity
+		new Handler ().postDelayed (new Runnable ()
+		{
+		/**
+		 * Show the splash screen.
+		 */
+		@Override
+		public void run ()
+			{
+			// This starts the main activity once the timer is up
+			Intent intent = new Intent (CSplashScreen.this, MainActivity.class);
+			startActivity (intent);
+			// Close this activity
+			finish ();
+			}
+		}, SPLASH_TIME_OUT);
+		}
+	}
